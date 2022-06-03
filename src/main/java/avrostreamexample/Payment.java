df73c05ed189de359bc42152a9482aa8cc9b478d
Aaron Ploetz <aaron.ploetz@datastax.com>
2022-06-03 15:47:21 -0500

    initial commit ...no errors

---
+++ src/main/java/avrostreamexample/Payment.java
@@ -0,0 +1,28 @@
+package avrostreamexample;
+
+public class Payment {
+
+	private String id;
+	private Double amount;
+	
+	public Payment(String id, Double amount) {
+		this.id = id;
+		this.amount = amount;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+}
